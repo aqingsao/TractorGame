6b3fb2d1d9b602ef5892f00a642e0f2da4f717c7
xqzhang <xqzhang@thoughtworks.com>
2012-08-26 16:39:44 +0800

    Refactor flipping

--- public/javascripts/app/flipping.js
+++ public/javascripts/app/flipping.js
@@ -1,78 +1,67 @@
 define(['common', 'app/cards'], function(Common, Cards){
+	 var check = function(jokers, trumps, currentRank){
+	 	if(jokers.size() == 0 || jokers.size > 2 || trumps.size() > 2){
+	 		return -1;  
+	 	}
+	 	if(jokers.size() == 1){
+	 		if(trumps.size() == 0){
+	 			return -1;
+	 		}          
+	
+	 		if(trumps.size() == 1){
+	 			if(jokers.at(0).isSmallJoker() && trumps.at(0).isBlackSuit() ){
+	 				return -1;
+	 			}
+	 			else if(jokers.at(0).isBigJoker() && trumps.at(0).isRedSuit() ){
+	 				return -1;
+	 			}
+	 			else if(currentRank != undefined && !trumps.at(0).rank.equals(currentRank)){
+	 				return -1;
+	 			} 
+  				return 1;
+	 		}
+	 	   	else{
+	 			if(!trumps.at(0).equals(trumps.at(1))){
+	 				return -1;
+	 			}       
+	 			else if(jokers.at(0).isSmallJoker() && trumps.at(0).isBlackSuit() ){
+	 				return -1;
+	 			}
+	 			else if(jokers.at(0).isBigJoker() && trumps.at(0).isRedSuit() ){
+	 				return -1;
+	 			} 
+	 			else if(currentRank != undefined && !trumps.at(0).rank.equals(currentRank)){
+	 				return -1;
+	 			} 
+	 			return 2;
+	 		}
+	 	}
+	 	else{// 2 jokers
+	 		if(!jokers.at(0).equals(jokers.at(1))){
+ 				return -1;
+	 		}
+	 		else if(trumps.size() > 0){
+ 				return -1;
+	 		}
+	 		else{
+	 			if(jokers.at(0).isSmallJoker()){
+	 				return 5;   // 2 small jokers
+	 			}
+  				return 10; //2 big jokers
+	 		}  
+	 	}
+	 }; 
+	
 	var Flipping = Common.Backbone.Model.extend({
 		initialize: function(player, cards, currentRank){     
 		 	var jokers = Cards.cards(cards.filter(function(card){return card.isJoker();}));
 		 	var trumps = Cards.cards(cards.reject(function(card){return card.isJoker();})); 
 		
 		 	this.set({jokers: jokers, trumps: trumps, banker: player, currentRank: currentRank}); 
-			this.check(this.get('jokers'), this.get('trumps'), this.get('currentRank'));
+			this.level = check(this.get('jokers'), this.get('trumps'), this.get('currentRank'));
 		 }, 
 		 isValid: function(){ 
-		 	return this.valid;
-		 }, 
-		 check: function(jokers, trumps, currentRank){
-		 	if(jokers.size() == 0 || jokers.size > 2 || trumps.size() > 2){
-		 		this.valid = false;  
-		 		return;
-		 	}
-		 	if(jokers.size() == 1){
-		 		if(trumps.size() == 0){
-		 			this.valid = false; 
-		 			return;
-		 		}          
-		
-		 		if(trumps.size() == 1){
-		 			if(jokers.at(0).isSmallJoker() && trumps.at(0).isBlackSuit() ){
-		 				this.valid = false;
-		 			}
-		 			else if(jokers.at(0).isBigJoker() && trumps.at(0).isRedSuit() ){
-		 				this.valid = false; 
-		 			}
-		 			else if(currentRank != undefined && !trumps.at(0).rank.equals(currentRank)){
-		 				this.valid = false; 
-		 			} 
-		 			else{   
-		 				this.valid = true; 
-		 				this.level = 1;
-		 			}
-		 		}
-		 	   	else{
-		 			if(!trumps.at(0).equals(trumps.at(1))){
-		 				this.valid = false;   
-		 			}       
-		 			else if(jokers.at(0).isSmallJoker() && trumps.at(0).isBlackSuit() ){
-		 				this.valid = false;				
-		 			}
-		 			else if(jokers.at(0).isBigJoker() && trumps.at(0).isRedSuit() ){
-		 				this.valid = false;  
-		 			} 
-		 			else if(currentRank != undefined && !trumps.at(0).rank.equals(currentRank)){
-		 				this.valid = false; 
-		 			} 
-		 			else{
-		 				this.valid = true;
-		 				this.level = 2;
-		 			}
-		 		}
-		 	}
-		 	else{// 2 jokers
-		 		if(!jokers.at(0).equals(jokers.at(1))){
-		 			this.valid = false;
-		 		}
-		 		else if(trumps.size() > 0){
-		 			this.valid = false;
-		 		}
-		 		else{
-		 			if(jokers.at(0).isSmallJoker()){
-		 				this.valid = true;
-		 				this.level = 5;   // 2 small jokers
-		 			}
-		 			else{
-		 				this.valid = true;
-		 				this.level = 10; //2 big jokers
-		 			}
-		 		}  
-		 	}
+		 	return this.level > 0;
 		 }, 
 		 canOverturn: function(flipping){
 		 	return this.level > flipping.level;
